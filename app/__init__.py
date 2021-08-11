0b31a20a4b42ea8835f596a0ddd8bcf7f66492ae
varsha <63990614+varsharathore16@users.noreply.github.com>
2021-08-10 22:15:11 -0400

    Updated endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,90 +1,96 @@
 import os
-from flask import Flask, Response, request, render_template, send_from_directory
-from dotenv import load_dotenv
-from . import db
-from werkzeug.security import generate_password_hash, check_password_hash
-from app.db import get_db
+from flask import Flask, render_template, request
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-load_dotenv()
+# from . import db
+# from app.db import get_db
+
 app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=5432,
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="Varsha Rathore", url="localhost:5000")
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health")
 def health():
-    return Response("HTTP 200 OK", mimetype='text/html'), 200
+    varsha = UserModel.query.filter_by(username="varsha").first()
+    has_varsha = "yes" if varsha is not None else "no"
+    return f"Works, has_varsha: {has_varsha}"
 
-@app.route('/register', methods=('GET', 'POST'))
+
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        db = get_db()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
-        elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
+            error = "Password is required."
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute('INSERT INTO user (username, password) VALUES (?, ?)', (username, generate_password_hash(password)))
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
-        
-    ## TODO: Return a restister page
-    return "Register Page not yet implemented", 501
 
-@app.route('/login', methods=('GET', 'POST'))
+    return render_template("register.html", title="Register")
+
+
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        db = get_db()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
-        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+            error = "Incorrect username."
+        elif not check_password_hash(user.password, password):
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
         else:
             return error, 418
 
-    # TODO: Return a login page
-    return "Login Page not yet implemented", 501
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return render_template("login.html", title="Login")
